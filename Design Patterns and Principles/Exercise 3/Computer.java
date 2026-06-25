cfd84aa58f3726b1d109602316e57da0cf1ad97a
Prerna Premkumar <prernaomega@gmail.com>
2026-06-25 22:35:05 +0530

    Added Exercise 3: Builder Pattern

---
+++ Design Patterns and Principles/Exercise 3/Computer.java
@@ -0,0 +1,46 @@
+public class Computer {
+    private String cpu;
+    private String ram;
+    private String storage;
+    private String gpu;
+
+    private Computer(Builder builder) {
+        this.cpu= builder.cpu;
+        this.ram= builder.ram;
+        this.storage= builder.storage;
+        this.gpu= builder.gpu;
+    }
+    public void displayConfiguration() {
+        System.out.println("CPU : " + cpu);
+        System.out.println("RAM : " + ram);
+        System.out.println("Storage : " + storage);
+        System.out.println("GPU : " + gpu);
+        System.out.println();
+    }
+    public static class Builder {
+        private String cpu;
+        private String ram;
+        private String storage;
+        private String gpu;
+
+        public Builder setCpu(String cpu) {
+            this.cpu= cpu;
+            return this;
+        }
+        public Builder setRam(String ram) {
+            this.ram= ram;
+            return this;
+        }
+        public Builder setStorage(String storage) {
+            this.storage= storage;
+            return this;
+        }
+        public Builder setGpu(String gpu) {
+            this.gpu= gpu;
+            return this;
+        }
+        public Computer build() {
+            return new Computer(this);
+        }
+    }
+}
