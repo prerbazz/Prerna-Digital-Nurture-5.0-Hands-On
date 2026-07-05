5855c60fe32b9e86683f8a3b3217c17abcb6c7c8
Prerna Premkumar <prernaomega@gmail.com>
2026-07-05 20:13:18 +0530

    Added Exercise 9: Command Pattern

---
+++ Design Patterns and Principles/Exercise 9/RemoteControl.java
@@ -0,0 +1,9 @@
+public class RemoteControl {
+    private Command cmd;
+    public void setCommand(Command cmd) {
+        this.cmd=cmd;
+    }
+    public void pressButton() {
+        cmd.execute();
+    }
+}
